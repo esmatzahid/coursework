ebb269d38d93949051e945ce202fedcf07195fe3
esmatzahid <148464706+esmatzahid@users.noreply.github.com>
2023-11-30 17:56:59 +0000

    Update basket.php
    added clear button to clear the basket, also added button to get you back to the home screen

--- basket.php
+++ basket.php
@@ -60,6 +60,16 @@
       list-style: none;
       padding: 0;
     }
+
+    .back-button {
+      display: inline-block;
+      margin-top: 20px;
+      padding: 10px 20px;
+      background-color: #333;
+      color: white;
+      text-decoration: none;
+      border-radius: 4px;
+    }
   </style>
 </head>
 <body>
@@ -69,32 +79,55 @@
 
   <!-- Featured Books Section -->
   <?php
-  // Fetch and display featured books from your backend
-  $featuredBooks = [
-    ["Book Title 1", "Author 1", 10.99, "https://example.com/book1.jpg"],
-    ["Book Title 2", "Author 2", 15.99, "https://example.com/book2.jpg"],
-    // Add more books as needed
-  ];
+    $host = "localhost";
+    $username = "2015319";
+    $password = "4d5c29";
+    $database = "db2015319";
 
-  foreach ($featuredBooks as $book) {
-    echo '<div class="book">';
-    echo '<img src="' . $book[3] . '" alt="' . $book[0] . '">';
-    echo '<div class="book-info">';
-    echo '<h3>' . $book[0] . '</h3>';
-    echo '<p>Author: ' . $book[1] . '</p>';
-    echo '<p>Price: £' . $book[2] . '</p>';
-    echo '<button onclick="addToBasket(\'' . $book[0] . '\', ' . $book[2] . ')">Add to Basket</button>';
-    echo '</div>';
-    echo '</div>';
-  }
+    // Create connection
+    $conn = new mysqli($host, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Retrieve data from the database
+    $sql = "SELECT * FROM books";
+    $result = $conn->query($sql);
+
+    // Check if the query was successful
+    if ($result === false) {
+      echo "Error: " . $conn->error;
+    } else {
+      // Display books
+      while ($row = $result->fetch_assoc()) {
+        echo '<div class="book">';
+        echo '<img src="' . $row["image"] . '" alt="' . $row["title"] . '">';
+        echo '<div class="book-info">';
+        echo '<h3>' . $row["title"] . '</h3>';
+        echo '<p>Author: ' . $row["author"] . '</p>';
+        echo '<p>Price: £' . $row["price"] . '</p>';
+        echo '<button onclick="addToBasket(\'' . $row["title"] . '\', ' . $row["price"] . ')">Add to Basket</button>';
+        echo '</div>';
+        echo '</div>';
+      }
+    }
+
+    // Close the connection
+    $conn->close();
   ?>
 
   <!-- Your Basket Section -->
   <div id="basket-list"></div>
   <button onclick="clearBasket()">Clear Basket</button>
+
+  <!-- Back Button -->
+  <a href="books.php" class="back-button">Back to Books</a>
 </div>
 
 <script>
+  // Your existing JavaScript code
   // Function to add a book to the basket
   function addToBasket(title, price) {
     // Create a new list item for the basket
@@ -107,13 +140,62 @@
 
     // Append the book info to the basket list
     document.getElementById("basket-list").appendChild(listItem);
+
+    // Save the added book to localStorage
+    saveBasketToLocalStorage();
   }
 
   // Function to clear the basket
   function clearBasket() {
     // Clear the basket list
     document.getElementById("basket-list").innerHTML = "";
+
+    // Clear the basket data from localStorage
+    localStorage.removeItem("basket");
   }
+
+  // Function to save basket data to localStorage
+  function saveBasketToLocalStorage() {
+    // Get the current basket data from localStorage
+    var basketData = localStorage.getItem("basket");
+
+    // Parse the basket data into an array or create an empty array if it doesn't exist
+    var basketArray = basketData ? JSON.parse(basketData) : [];
+
+    // Add the new book to the basket array
+    basketArray.push({
+      title: title,
+      price: price
+    });
+
+    // Save the updated basket data back to localStorage
+    localStorage.setItem("basket", JSON.stringify(basketArray));
+  }
+
+  // Function to load basket data from localStorage
+  function loadBasketFromLocalStorage() {
+    // Get the basket data from localStorage
+    var basketData = localStorage.getItem("basket");
+
+    // Parse the basket data into an array or create an empty array if it doesn't exist
+    var basketArray = basketData ? JSON.parse(basketData) : [];
+
+    // Display the books in the basket
+    basketArray.forEach(function (book) {
+      addToBasket(book.title, book.price);
+    });
+  }
+
+  // Load the basket data when the page is loaded
+  window.onload = function () {
+    loadBasketFromLocalStorage();
+  };
+</script>
+
+
+</body>
+</html>
+
 </script>
 
 </body>
